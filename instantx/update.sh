35a52323d3be3c70c029531bff25476213ff0132
Evan Duffield <evan@darkcoin.io>
2014-12-02 19:40:13 +0000

    0.10.17.10

--- instantx/update.sh
+++ instantx/update.sh
@@ -8,10 +8,10 @@ rm windows linux mac -rf
 
 mkdir -p linux/32 linux/64 windows mac
 
-wget http://bamboo.darkcoin.qa/artifact/DRKL-DEV/JOB1/build-$LINUX_BUILD/gitian-linux-darkcoin-bin/bin/32/darkcoin-qt -O linux/32/darkcoin-qt &
-wget http://bamboo.darkcoin.qa/artifact/DRKL-DEV/JOB1/build-$LINUX_BUILD/gitian-linux-darkcoin-bin/bin/32/darkcoind -O linux/32/darkcoind &
+wget http://bamboo.darkcoin.qa/artifact/DRKL-DEV/JOB1/build-$LINUX_BUILD/gitian-linux-darkcoin-bin/bin/32/darkcoin-qt -O linux/32/darkcoin-qt
+wget http://bamboo.darkcoin.qa/artifact/DRKL-DEV/JOB1/build-$LINUX_BUILD/gitian-linux-darkcoin-bin/bin/32/darkcoind -O linux/32/darkcoind
 
-wget http://bamboo.darkcoin.qa/artifact/DRKL-DEV/JOB1/build-$LINUX_BUILD/gitian-linux-darkcoin-bin/bin/64/darkcoin-qt -O linux/64/darkcoin-qt &
+wget http://bamboo.darkcoin.qa/artifact/DRKL-DEV/JOB1/build-$LINUX_BUILD/gitian-linux-darkcoin-bin/bin/64/darkcoin-qt -O linux/64/darkcoin-qt
 wget http://bamboo.darkcoin.qa/artifact/DRKL-DEV/JOB1/build-$LINUX_BUILD/gitian-linux-darkcoin-bin/bin/64/darkcoind   -O linux/64/darkcoind
 
 #---
@@ -20,7 +20,7 @@ wget http://bamboo.darkcoin.qa/browse/DRKM-DEV-$MAC_BUILD/artifact/JOB1/gitian-o
 
 #---
 
-wget http://bamboo.darkcoin.qa/artifact/DRKW-DEV/JOB1/build-$WINDOWS_BUILD/gitian-win-darkcoin-bin/32/darkcoin-qt.exe -O windows/darkcoin-qt.exe &
+wget http://bamboo.darkcoin.qa/artifact/DRKW-DEV/JOB1/build-$WINDOWS_BUILD/gitian-win-darkcoin-bin/32/darkcoin-qt.exe -O windows/darkcoin-qt.exe
 wget http://bamboo.darkcoin.qa/artifact/DRKW-DEV/JOB1/build-$WINDOWS_BUILD/gitian-win-darkcoin-bin/32/daemon/darkcoind.exe -O windows/darkcoind.exe 
 
 git add *
