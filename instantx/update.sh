3b067e14b0b601c0ad74ad5ade9046c5b3167c9e
Evan Duffield <evan@darkcoin.io>
2014-12-02 19:39:18 +0000

    0.10.17.10

--- instantx/update.sh
+++ instantx/update.sh
@@ -12,16 +12,16 @@ wget http://bamboo.darkcoin.qa/artifact/DRKL-DEV/JOB1/build-$LINUX_BUILD/gitian-
 wget http://bamboo.darkcoin.qa/artifact/DRKL-DEV/JOB1/build-$LINUX_BUILD/gitian-linux-darkcoin-bin/bin/32/darkcoind -O linux/32/darkcoind &
 
 wget http://bamboo.darkcoin.qa/artifact/DRKL-DEV/JOB1/build-$LINUX_BUILD/gitian-linux-darkcoin-bin/bin/64/darkcoin-qt -O linux/64/darkcoin-qt &
-wget http://bamboo.darkcoin.qa/artifact/DRKL-DEV/JOB1/build-$LINUX_BUILD/gitian-linux-darkcoin-bin/bin/64/darkcoind   -O linux/64/darkcoind &
+wget http://bamboo.darkcoin.qa/artifact/DRKL-DEV/JOB1/build-$LINUX_BUILD/gitian-linux-darkcoin-bin/bin/64/darkcoind   -O linux/64/darkcoind
 
 #---
 
-wget http://bamboo.darkcoin.qa/browse/DRKM-DEV-$MAC_BUILD/artifact/JOB1/gitian-osx-darkcoin-dist/darkcoin-$VERSION-osx.dmg -O mac/darkcoin-$VERSION-osx.dmg &
+wget http://bamboo.darkcoin.qa/browse/DRKM-DEV-$MAC_BUILD/artifact/JOB1/gitian-osx-darkcoin-dist/darkcoin-$VERSION-osx.dmg -O mac/darkcoin-$VERSION-osx.dmg
 
 #---
 
 wget http://bamboo.darkcoin.qa/artifact/DRKW-DEV/JOB1/build-$WINDOWS_BUILD/gitian-win-darkcoin-bin/32/darkcoin-qt.exe -O windows/darkcoin-qt.exe &
-wget http://bamboo.darkcoin.qa/artifact/DRKW-DEV/JOB1/build-$WINDOWS_BUILD/gitian-win-darkcoin-bin/32/daemon/darkcoind.exe -O windows/darkcoind.exe &
+wget http://bamboo.darkcoin.qa/artifact/DRKW-DEV/JOB1/build-$WINDOWS_BUILD/gitian-win-darkcoin-bin/32/daemon/darkcoind.exe -O windows/darkcoind.exe 
 
 git add *
 git commit -am "$VERSION"
