0d2b0d22739cfde2c389007952bf44d99428aba5
Evan Duffield <evan@darkcoin.io>
2015-03-25 10:13:03 -0700

    0.11.1.26

--- v11/update.sh
+++ v11/update.sh
@@ -1,33 +1,33 @@
 
-LINUX_BUILD=91
-MAC_BUILD=74
-WINDOWS_BUILD=86
-VERSION='0.11.0.6'
+LINUX_BUILD=168
+MAC_BUILD=134
+WINDOWS_BUILD=157
+VERSION='0.11.1.26'
 GPG='986FF288'
 
-rm darkcoin-*
+rm dash-*
 
-wget http://bamboo.darkcoin.qa/browse/DRKL-DEV-$LINUX_BUILD/artifact/JOB1/gitian-linux-darkcoin-dist/darkcoin-$VERSION-linux.tar.gz -O darkcoin-$VERSION-linux.tar.gz
-wget http://bamboo.darkcoin.qa/browse/DRKL-DEV-$LINUX_BUILD/artifact/JOB1/gitian-linux-darkcoin-DIGESTS/darkcoin-$VERSION-linux.tar.gz.DIGESTS.txt -O darkcoin-$VERSION-linux.tar.gz.DIGESTS.txt
-
-#---
-
-wget http://bamboo.darkcoin.qa/browse/DRKM-DEV-$MAC_BUILD/artifact/JOB1/gitian-osx-darkcoin-dist/darkcoin-$VERSION-osx.dmg -O darkcoin-$VERSION-osx.dmg
-wget http://bamboo.darkcoin.qa/browse/DRKM-DEV-$MAC_BUILD/artifact/JOB1/gitian-osx-darkcoin-DIGESTS/darkcoin-$VERSION-osx.dmg.DIGESTS.txt -O darkcoin-$VERSION-osx.dmg.DIGESTS.txt
+wget http://bamboo.dash.qa/browse/DRKL-DEV-$LINUX_BUILD/artifact/JOB1/gitian-linux-dash-dist/dash-$VERSION-linux.tar.gz -O dash-$VERSION-linux.tar.gz
+wget http://bamboo.dash.qa/browse/DRKL-DEV-$LINUX_BUILD/artifact/JOB1/gitian-linux-dash-DIGESTS/dash-$VERSION-linux.tar.gz.DIGESTS.txt -O dash-$VERSION-linux.tar.gz.DIGESTS.txt
 
 #---
 
-wget http://bamboo.darkcoin.qa/browse/DRKW-DEV-$WINDOWS_BUILD/artifact/JOB1/gitian-win-darkcoin-dist/darkcoin-$VERSION-win.zip -O darkcoin-$VERSION-win.zip
-wget http://bamboo.darkcoin.qa/artifact/DRKW-DEV/JOB1/build-86/gitian-win-darkcoin-bin/64/darkcoin-0.11.0-win64-setup.exe -O darkcoin-$VERSION-win32-setup.exe
-wget http://bamboo.darkcoin.qa/artifact/DRKW-DEV/JOB1/build-$WINDOWS_BUILD/gitian-win-darkcoin-DIGESTS/darkcoin-$VERSION-win.zip.DIGESTS.txt -O darkcoin-$VERSION-win.zip.DIGESTS.txt
-wget http://bamboo.darkcoin.qa/artifact/DRKW-DEV/JOB1/build-$WINDOWS_BUILD/gitian-win-darkcoin-DIGESTS/darkcoin-$VERSION-win32-setup.exe.DIGESTS.txt -O darkcoin-$VERSION-win32-setup.exe.DIGESTS.txt
+wget http://bamboo.dash.qa/browse/DRKM-DEV-$MAC_BUILD/artifact/JOB1/gitian-osx-dash-dist/dash-$VERSION-osx.dmg -O dash-$VERSION-osx.dmg
+wget http://bamboo.dash.qa/browse/DRKM-DEV-$MAC_BUILD/artifact/JOB1/gitian-osx-dash-DIGESTS/dash-$VERSION-osx.dmg.DIGESTS.txt -O dash-$VERSION-osx.dmg.DIGESTS.txt
 
 #---
 
-gpg -b --armor -u $GPG --no-emit-version darkcoin-$VERSION-linux.tar.gz
-gpg -b --armor -u $GPG --no-emit-version darkcoin-$VERSION-win32-setup.exe
-gpg -b --armor -u $GPG --no-emit-version darkcoin-$VERSION-win.zip
-gpg -b --armor -u $GPG --no-emit-version darkcoin-$VERSION-osx.dmg
+wget http://bamboo.dash.qa/browse/DRKW-DEV-$WINDOWS_BUILD/artifact/JOB1/gitian-win-dash-dist/dash-$VERSION-win.zip -O dash-$VERSION-win.zip
+wget http://bamboo.dash.qa/artifact/DRKW-DEV/JOB1/build-86/gitian-win-dash-bin/64/dash-0.11.0-win64-setup.exe -O dash-$VERSION-win32-setup.exe
+wget http://bamboo.dash.qa/artifact/DRKW-DEV/JOB1/build-$WINDOWS_BUILD/gitian-win-dash-DIGESTS/dash-$VERSION-win.zip.DIGESTS.txt -O dash-$VERSION-win.zip.DIGESTS.txt
+wget http://bamboo.dash.qa/artifact/DRKW-DEV/JOB1/build-$WINDOWS_BUILD/gitian-win-dash-DIGESTS/dash-$VERSION-win32-setup.exe.DIGESTS.txt -O dash-$VERSION-win32-setup.exe.DIGESTS.txt
+
+#---
+
+gpg -b --armor -u $GPG --no-emit-version dash-$VERSION-linux.tar.gz
+gpg -b --armor -u $GPG --no-emit-version dash-$VERSION-win32-setup.exe
+gpg -b --armor -u $GPG --no-emit-version dash-$VERSION-win.zip
+gpg -b --armor -u $GPG --no-emit-version dash-$VERSION-osx.dmg
 
 git add *
 git commit -am "$VERSION"
